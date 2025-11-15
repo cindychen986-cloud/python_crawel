485be15838b12e4b7f48b1c43f7bfdbc1d509dd3
cindychen986-cloud <cindychen986@gmail.com>
2025-11-15 16:49:28 +0800

    11152025-5

--- lesson3/lesson3_4.py
+++ lesson3/lesson3_4.py
@@ -1,28 +1,40 @@
 import random
 
-min=1
-max=50
-count=0
-target=random.randint(min,max)
-print("猜數字遊戲\n")
-while True:
-    keyin=int(input(f"猜數字範圍{min}~{max}:"))
-    count+=1
-    if keyin >= min and keyin <=max:
-        if target == keyin:
-            print(f"Bingo! 答案是:{target}")
-            print(f"你猜了{count}次")
+def play_game():
+        min = 1
+        max = 100
+        count = 0
+        target = random.randint(min,max)
+        print(target)
+        print("============猜數字遊戲===========\n")
+        while(True):
+            keyin = int(input(f"猜數字範圍{min}~{max}:"))
+            count += 1
+            if keyin >= min and keyin <=max:
+                if target == keyin:
+                    print(f"賓果!猜對了, 答案是:{target}")
+                    print(f"您猜了{count}")
+                    break
+                elif (keyin > target):
+                    print("再小一點")
+                    max = keyin - 1
+                elif (keyin < target):
+                    print("再大一點")
+                    min = keyin + 1
+                print(f"您已經猜了{count}")
+                    
+            else:
+                print("請輸入提示範圍內的數字")
+
+def main():
+    while(True):
+        play_game()
+        
+        is_play_again = input("您還要繼續嗎?y,n")
+        if is_play_again == 'n':
             break
-        elif(keyin>target):
-            print("再小一點")
-            max= keyin-1
-        elif(keyin<target):
-            print("再大一點")
-            min= keyin+1
-        print(f"您已經猜了{count}次了")
-    else:
-        print("請輸入提示範圍內的數字")
-    playagain=input("還要再玩一次嗎?")
-    if playagain=='n':
-        break
-print("遊戲結束")
+        
+    print("遊戲結束")
+
+if __name__=="__main__":
+    main()
